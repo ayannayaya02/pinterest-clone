280c3f31b8a886861eb0bc2e63e6f6533cbf26d5
ayanna <ayannafofana.aaf@gmail.com>
2023-07-29 19:19:46 -0700

    creating header
    includes...
    -pinterest logo
    -search engine/bar
    -homepage button
    -following button
    -other icons

--- src/ingredients/Header.js
+++ src/ingredients/Header.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import PinterestIcon from '@material-ui/icons/Pinterest'
 
 function Header() {
   return (
     <div>
-      
+      <Wrapper>
+
+      <LogoWrapper>
+        <Icon>
+          <PinterestIcon/>
+
+        </Icon>
+      </LogoWrapper>
+
+      <Following>
+      </Following>
+
+      <SearchEngine>
+      </SearchEngine>
+
+
+      </Wrapper>
     </div>
   );
 }
 
 export default Header
+ 
+const Wrapper=styled.div`
+display:flex;
+align-items:center;
+height:56px;
+padding:12px 5px 5px 15px;
+background-color:#ffff;
+color:black;
+`
