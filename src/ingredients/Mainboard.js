a63f4b961a49e8fc4916fada64dec08f7853942c
ayanna <ayannafofana.aaf@gmail.com>
2023-08-14 01:24:01 -0700

    updates for pins
    mobile devices where pins will be evenly divided with two columns

--- src/ingredients/Mainboard.js
+++ src/ingredients/Mainboard.js
@@ -31,14 +31,19 @@ justify-content:center;
 display:flex;
 `
 
-const Container=styled.div`
-background-color:#fffff;
-width:80%;
-column-count:4;
-column-gap:50px;
-margin:0 auto;
-background-color: #fffff;
-height:100%;
-max-width:1300px;
+const Container = styled.div`
+  @media (max-width: 1763px) {
+    column-count: 2;
+    max-width: 584px;
+    column-gap:50;
+  }
 
-`
+  background-color: #fffff;
+  width: 80%;
+  column-count: 4;
+  column-gap: 50px;
+  margin: 0 auto;
+  background-color: #fffff;
+  height: 100%;
+  max-width: 1300px;
+`;
