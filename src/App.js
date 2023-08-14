ac8552b31621e37dee20f62a339c1165cd8583ef
ayanna <ayannafofana.aaf@gmail.com>
2023-08-13 20:46:58 -0700

    i think I figured it out!!
    Javascript to make images appear!!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import React,{useState} from "react";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 import Header from "./ingredients/Header";
 import Mainboard from "./ingredients/Mainboard";
 import splash from "./api/splash";
@@ -9,58 +9,72 @@ import "./App.css";
 /*For any components don't forget to also import them above! */
 
 function App() {
-  const[pins,setNewPins]=useState([])
+  const [pins, setNewPins] = useState([]);
 
-const getImages = (term) =>{
-return splash.get("https://api.unsplash.com/search/photos/", {
-params:{
-  query:term
-}
-});
-};
+  const getImages = (term) => {
+    return splash.get("https://api.unsplash.com/search/photos/", {
+      params: {
+        query: term,
+      },
+    });
+  };
 
-const onSearchSubmit=(term)=>{
-console.log(term)
-  getImages(term).then((res)=>{
-    let results=res.data.results;
+  const onSearchSubmit = (term) => {
+    console.log(term);
+    getImages(term).then((res) => {
+      let results = res.data.results;
 
-    let newNew=[
-      ...results,
-      ...pins,
-    ]
+      let newNew = [...results, ...pins];
 
-    newNew.sort(function(a,b) {
-      return 0.5-Math.random();
+      newNew.sort(function (a, b) {
+        return 0.5 - Math.random();
       });
 
       setNewPins(newNew);
-})
-}
-const getNewPins= () =>{
-let promises=[];
-let pinData=[];
-let pins=['ocean','Paris', 'dogs', 'wolves', 'hummingbirds', 'martial arts', 'food', 'travel']
+    });
+  };
+  const getNewPins = () => {
+    let promises = [];
+    let pinData = [];
+    let pins = [
+      "ocean",
+      "Paris",
+      "dogs",
+      "wolves",
+      "hummingbirds",
+      "martial arts",
+      "food",
+      "travel",
+    ];
 
-pins.forEach((pinTerm)=> {
-promises.push(
-  getImages(pinTerms)
-)
-})
+    pins.forEach((pinTerm) => {
+      promises.push(
+        getImages(pinTerm).then((res) => {
+          let results = res.data.results;
+          pinData = pinData.concat(results);
+          pinData.sort(function (a, b) {
+            return 0.5 - Math.random();
+          });
+        })
+      );
+    });
+    Promise.all(promises).then(() => {
+      setNewPins(pinData);
+    });
+  };
 
+  useEffect(() => {
+    getNewPins();
+  }, []);
 
-useEffect(()=>
-{
-  getNewPins(),
-
-}, [])
   return (
     <div className="App">
       <header className="App-header">
-     <Header onSubmit={onSearchSubmit}/>
-     <Mainboard pins={pins}/>
+        <Header onSubmit={onSearchSubmit} />
+        <Mainboard pins={pins} />
       </header>
     </div>
   );
-  }
+}
 
 export default App;
